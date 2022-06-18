6aeffde03f6e15a04954202faa0ea67ccaebe462
lilromuill <roma12garmaash@gmail.com>
2022-06-18 23:49:11 +0300

    requah

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 BOT_TOKEN = "5097991747:AAENlPBgr1cywkyhaB7JoIH5Vm1a9InamXA"
 APP_URL = "https://romuilldb.herokuapp.com/" + BOT_TOKEN
+DB_URI = "postgres://xumeafkccrmyoo:1d168594db48d4f8f08342c545c39e5daa465b4a54ea20b97d92e4dddd7ad66e@ec2-52-73-184-24.compute-1.amazonaws.com:5432/d9vjdmb74rsfqt"
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import telebot
 import logging
+import psycopg2
 from config import *
 from flask import Flask, request
 
@@ -9,12 +10,51 @@ server = Flask(__name__)
 logger = telebot.logger
 logger.setLevel(logging.DEBUG)
 
+db_connection = psycopg2.connect(DB_URI, sslmode="require")
+db_object = db_connection.cursor()
 
-@bot.message_handler(commands=['start'])
+
+def update_messages_count(user_id):
+    db_object.execute(f"UPDATE users SET messages = messages + 1 WHERE id = {user_id}")
+    db_connection.commit()
+
+
+@bot.message_handler(commands=["start"])
 def start(message):
-    bot.send_message(message.chat.id, 'It works!')
+    user_id = message.from_user.id
+    username = message.from_user.username
+    bot.reply_to(message, f"Hello, {username}!")
+
+    db_object.execute(f"SELECT id FROM users WHERE id = {user_id}")
+    result = db_object.fetchone()
+
+    if not result:
+        db_object.execute("INSERT INTO users(id, username, messages) VALUES (%s, %s, %s)", (user_id, username, 0))
+        db_connection.commit()
+
+    update_messages_count(user_id)
 
 
+@bot.message_handler(commands=["stats"])
+def get_stats(message):
+    db_object.execute("SELECT * FROM users ORDER BY messages DESC LIMIT 10")
+    result = db_object.fetchall()
+
+    if not result:
+        bot.reply_to(message, "No data...")
+    else:
+        reply_message = "- Top flooders:\n"
+        for i, item in enumerate(result):
+            reply_message += f"[{i + 1}] {item[1].strip()} ({item[0]}) : {item[2]} messages.\n"
+        bot.reply_to(message, reply_message)
+
+    update_messages_count(message.from_user.id)
+
+
+@bot.message_handler(func=lambda message: True, content_types=["text"])
+def message_from_user(message):
+    user_id = message.from_user.id
+    update_messages_count(user_id)
 
 
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
@@ -28,10 +68,5 @@ def redirect_message():
 if __name__ == "__main__":
     bot.remove_webhook()
     bot.set_webhook(url=APP_URL)
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 4999)))
-
-
-
-
-
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 4999)))
 
